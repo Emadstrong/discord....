b3b8987eff0791cb7cfaf3697ca2bd83b42b5955
Emadstrong <132583034+Emadstrong@users.noreply.github.com>
2024-02-20 14:15:30 +0300

    Update defaults.go

--- embedg-server/config/defaults.go
+++ embedg-server/config/defaults.go
@@ -8,24 +8,24 @@ func setupDefaults() {
 	v.SetDefault("discord.activity_name", "message.style")
 
 	// Postgres defaults
-	v.SetDefault("postgres.host", "localhost")
+	v.SetDefault("postgres.host", "dpg-cn99c56d3nmc73dh8ma0-a")
 	v.SetDefault("postgres.port", 5432)
-	v.SetDefault("postgres.dbname", "embedg")
-	v.SetDefault("postgres.user", "postgres")
-	v.SetDefault("postgres.password", "")
+	v.SetDefault("postgres.dbname", "hellosjeejeww")
+	v.SetDefault("postgres.user", "hellosjeejeww_user")
+	v.SetDefault("postgres.password", "0jNqHD2FQmVbMKvhRkenJtYgig3TiVRo")
 
 	// S3 defaults
 	v.SetDefault("s3.endpoint", "localhost:9000")
 	v.SetDefault("s3.access_key_id", "embedg")
 	v.SetDefault("s3.secret_access_key", "1234567890")
 
-	v.SetDefault("app.public_url", "http://localhost:5173/app")
+	v.SetDefault("app.public_url", "https://ggffgfhayaya.onrender.com/app")
 
 	// API defaults
-	v.SetDefault("api.host", "localhost")
-	v.SetDefault("api.port", 8080)
-	v.SetDefault("api.public_url", "http://localhost:5173/api")
+	v.SetDefault("api.host", "https://ggffgfhayaya.onrender.com")
+	v.SetDefault("api.port", 10000)
+	v.SetDefault("api.public_url", "https://ggffgfhayaya.onrender.com/api")
 
 	// CDN defaults
-	v.SetDefault("cdn.public_url", "http://localhost:8080/cdn")
+	v.SetDefault("cdn.public_url", "https://ggffgfhayaya.onrender.com/cdn")
 }
